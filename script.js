f06ae254e50e078ad771b4872f7fc9fe062ee3ac
Helena Johansson <helena@klarr.se>
2021-12-09 17:31:11 +0100

    final code

--- script.js
+++ script.js
@@ -20,3 +20,39 @@ onLoad();
 function itemTemplate(item) {
   return `<li><span class="value">${item}</span> <button onclick="handleEdit(this)">Edit</button> <button onclick="handleDelete(this)">Delete</button></li>`;
 }
+
+// add eventlistener
+ourForm.addEventListener("submit", function (e) {
+  // listening to the form element and a new item gets submitted
+  e.preventDefault();
+  ourList.insertAdjacentHTML("beforeend", itemTemplate(ourField.value));
+  ourField.value = "";
+  saveData();
+});
+
+// edit item
+function handleEdit(el) {
+  let newValue = prompt(
+    "Enter a value",
+    el.parentElement.querySelector(".value").innerHTML
+  );
+  if (newValue) {
+    el.parentElement.querySelector(".value").innerHTML = newValue;
+  }
+  saveData();
+}
+
+// delete item
+function handleDelete(el) {
+  el.parentElement.remove();
+  saveData();
+}
+
+// save data
+function saveData() {
+  let allItems = [];
+  let simpleArray = ourList.querySelectorAll("li").forEach(function (el) {
+    allItems.push(el.querySelector(".value").innerHTML);
+  });
+  localStorage.setItem("domWayData", JSON.stringify(allItems));
+}
